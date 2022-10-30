3fc0a00ce738524e66e4e3b62273729df2f055b6
linashuk <106984021+linashuk@users.noreply.github.com>
2022-10-30 19:10:35 +0000

    project 3

---
+++ project-3/project-3.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// The prime factors of 13195 are 5, 7, 13 and 29.
+	// What is the largest prime factor of the number 600851475143 ?
+
+	number := 600851475143
+
+	slice := []int{}
+
+	for i := 1; i <= number; i++ {
+		if number%i == 0 {
+			slice = append(slice, i)
+			number = number / i
+		}
+	}
+
+	fmt.Println(slice[len(slice)-1])
+}
