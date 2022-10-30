94e442ba56bf8357cc29473c34e19067310e3b61
linashuk <106984021+linashuk@users.noreply.github.com>
2022-10-30 10:24:54 +0000

    message

--- project-2/project-2.go
+++ project-2/project-2.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Not sure yet.")
+
+	fibonacciSlice := []int{2}
+	fmt.Println(fibonacciSlice)
+
+	firstInt := 1
+	secondInt := 2
+	var thirdInt int
+
+	for firstInt+secondInt < 4000000 {
+		thirdInt = firstInt + secondInt
+		if thirdInt%2 == 0 {
+			fibonacciSlice = append(fibonacciSlice, thirdInt)
+		}
+		firstInt = secondInt
+		secondInt = thirdInt
+	}
+
+	fmt.Println(fibonacciSlice)
+
 }
